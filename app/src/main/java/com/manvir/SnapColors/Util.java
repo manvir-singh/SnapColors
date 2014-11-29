5cc138731b3893674d4b4736ed425f450dab1807
Manvir Singh <manvir@manvir.x10.mx>
2014-11-28 20:20:44 -0800

    Removed unused method.

--- app/src/main/java/com/manvir/SnapColors/Util.java
+++ app/src/main/java/com/manvir/SnapColors/Util.java
@@ -181,16 +181,4 @@ public class Util {
             return null;
         }
     }
-
-    public static int generateViewId() {
-        for (;;) {
-            final int result = sNextGeneratedId.get();
-            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
-            int newValue = result + 1;
-            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
-            if (sNextGeneratedId.compareAndSet(result, newValue)) {
-                return result;
-            }
-        }
-    }
 }
