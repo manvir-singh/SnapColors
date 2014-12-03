bcbb15ee2c9af155643bd4ad869e0bcd7d4cc78f
Manvir Singh <iphone4life1@gmail.com>
2014-12-02 20:51:20 -0800

    Fixed a possible memory leak or could lead to one.

--- app/src/main/java/com/manvir/SnapColors/MainActivity.java
+++ app/src/main/java/com/manvir/SnapColors/MainActivity.java
@@ -266,6 +266,7 @@ public class MainActivity extends Activity {
 								public void onClick(DialogInterface dialog, int which) {
 									prefs.edit().putBoolean("setFont", false).apply();
 									setFont.setChecked(false);
+                                    System.gc();
 								}
 							});
 
@@ -277,6 +278,7 @@ public class MainActivity extends Activity {
 					            	prefs.edit().putString("Font", strName).apply();
 					            	prefs.edit().putBoolean("setFont", true).apply();
 					            	fontPrev.setTypeface(face);
+                                    System.gc();
 					            }
 				            });
 				            builderSingle.show();
--- app/src/main/java/com/manvir/SnapColors/Sizelayout.java
+++ app/src/main/java/com/manvir/SnapColors/Sizelayout.java
@@ -1,7 +1,15 @@
 package com.manvir.SnapColors;
 
+import android.content.Context;
+import android.widget.LinearLayout;
+
 /**
  * Created by Manvir on 11/28/2014.
  */
-public class Sizelayout {
+public class Sizelayout extends LinearLayout{
+    public Sizelayout(Context context) {
+        super(context);
+        inflate(context, R.layout.size_layout, null);
+        
+    }
 }
