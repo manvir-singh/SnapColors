ef562f0b27363af3c785e5019909cbaf78dcecff
Manvir <webmaster@programming4life.com>
2015-06-21 18:42:50 -0700

    Reformat code

--- app/src/main/java/com/manvir/common/SETTINGS.java
+++ app/src/main/java/com/manvir/common/SETTINGS.java
@@ -2,11 +2,27 @@ package com.manvir.common;
 
 import android.graphics.Color;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class SETTINGS {
     public static final String NAME = "settings";
 
+    public static final class DEFAULTS {
+        public static final Set<String> blockStoriesFromList = new HashSet<>();
+        public static final boolean screenshotDetection = true;
+        public static final boolean autoRandomize = false;
+        public static final boolean shouldRainbow = false;
+        public static final int TextColor = Color.WHITE;
+        public static final boolean checkForVer = true;
+        public static final int BGColor = -1728053248;
+        public static final boolean setFont = false;
+        public static final int minTimerInt = 10;
+    }
+
     public final class KEYS {
         public static final String clearAllImportedFonts = "clearAllImportedFonts";
+        public static final String blockStoriesFromList = "blockStoriesFromList";
         public static final String screenshotDetection = "screenshotDetection";
         public static final String autoRandomize = "autoRandomize";
         public static final String shouldRainbow = "shouldRainbow";
@@ -17,16 +33,5 @@ public final class SETTINGS {
         public static final String BGColor = "BGColor";
         public static final String setFont = "setFont";
         public static final String donate = "donate";
-    }
-
-    public final class DEFAULTS {
-        public static final boolean screenshotDetection = true;
-        public static final boolean autoRandomize = false;
-        public static final boolean shouldRainbow = false;
-        public static final int TextColor = Color.WHITE;
-        public static final boolean checkForVer = true;
-        public static final int BGColor = -1728053248;
-        public static final boolean setFont = false;
-        public static final int minTimerInt = 10;
     }
 }
